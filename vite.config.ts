e31451e906cd712e20931974f769a4e44a0602fe
KyleTryon <git@techsquidtv.com>
2025-05-17 01:55:47 -0400

    ci: add build setup

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,21 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: 'src/components/tiktok-textbox.tsx',
+      name: 'TikTokTextbox',      // global var name for UMD
+      fileName: 'tiktok-textbox', // output file/dir prefix
+      formats: ['es', 'cjs', 'umd'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
 })
